cca9f5a11bb355f185f3dbd83923481c48426ff0
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-08 08:34:28 +0530

    firts commit

---
+++ 01-user-authentication-system/Controllers/userController.js
@@ -0,0 +1,10 @@
+import bcrypt from 'bcryptjs';
+
+// Login Controller
+export const login = async (req,res) => {
+    const {email,password} = req.body;
+}
+
+export const register = async (req,res) => {
+
+}
